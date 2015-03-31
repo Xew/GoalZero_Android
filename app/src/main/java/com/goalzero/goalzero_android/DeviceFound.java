e3888e9e8b8fd09712b1b774cb55d36bd2149163
Xew <xew1024@gmail.com>
2015-03-31 09:26:57 -0600

    Listing fake Devices

--- app/src/main/java/com/goalzero/goalzero_android/DeviceFound.java
+++ app/src/main/java/com/goalzero/goalzero_android/DeviceFound.java
@@ -1,19 +1,51 @@
 package com.goalzero.goalzero_android;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
 public class DeviceFound extends ActionBarActivity
 {
+
+	private String[] Devices;
+	private int selectedDevice = -1;
+	private AdapterView.OnItemClickListener itemClicked = new AdapterView.OnItemClickListener()
+	{
+		@Override
+		public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id)
+		{
+
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_device_found);
+
+		Devices = new String[2];
+		Devices[0] = "Yeti 150";
+		Devices[1] = "Yeti 151";
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Devices);
+
+		ListView devices = (ListView) findViewById(R.id.devicesListView);
+		devices.setAdapter(adapter);
+		devices.setOnItemClickListener(itemClicked);
 	}
 
 
